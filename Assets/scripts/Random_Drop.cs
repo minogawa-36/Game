e4809c604f670305fdcd37fe018cd1a811ea6ffc
tsukano22170292 <133092772+tsukano22170292@users.noreply.github.com>
2023-05-25 15:58:23 +0900

    ごみ３種類。カメラ内で落ちる変更

--- Assets/scripts/Random_Drop.cs
+++ Assets/scripts/Random_Drop.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class Random_Drop : MonoBehaviour
 {
     public GameObject objectPrefab; // オブジェクトのプレハブを格納する変数
-    public float spawnRangeX = 10f; // X軸の出現範囲
-    public float spawnRangeY = 12f; // Y軸の出現範囲
+    public GameObject objectPrefab2; //２個目のオブジェクト
+    public GameObject objectPrefab3;
+    public float spawnRangeX; // X軸の出現範囲
+    public float spawnRangeY; // Y軸の出現範囲
 
     void Start()
     {
+        Camera mainCamera = Camera.main;
+        float cameraHeight = 2f * mainCamera.orthographicSize;
+        float cameraWidth = cameraHeight * mainCamera.aspect;
+
+        spawnRangeX = cameraWidth;
+        spawnRangeY = cameraHeight;
+        
         InvokeRepeating("SpawnObject", 0f, 1.0f); // 1秒ごとに SpawnObject メソッドを呼び出す
     }
 
@@ -17,7 +26,21 @@ public class Random_Drop : MonoBehaviour
         float spawnPositionY = Random.Range(-spawnRangeY / 2f, spawnRangeY / 2f); // Y軸のランダムな座標を生成
         Vector2 spawnPosition = new Vector2(spawnPositionX, spawnPositionY);
 
-        Instantiate(objectPrefab, spawnPosition, Quaternion.identity); // オブジェクトを生成
+        int randomIndex = Random.Range(0, 3); //０または２のランダムな値を生成
+
+        GameObject obj = null; // GameObject オブジェクトの宣言と初期化
+
+        if (randomIndex == 0){
+            obj = Instantiate(objectPrefab, spawnPosition, Quaternion.identity); // オブジェクトを生成
+        }else if (randomIndex == 1){
+            obj = Instantiate(objectPrefab2, spawnPosition, Quaternion.identity); // オブジェクト２を生成
+        }else{
+            obj = Instantiate(objectPrefab3, spawnPosition, Quaternion.identity); // オブジェクト３を生成
+        }
+        
+        if (obj != null){
+            obj.SetActive(true); //表示する
+        }
+
     }
 }
-
